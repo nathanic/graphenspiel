e2d36ebea5c0bb230b30fe5b6ccbc8f43894cc34
Nathan Stien <nathanism@gmail.com>
2012-08-13 20:22:14 -0500

    WIP

--- src/quil_playground/circles.clj
+++ src/quil_playground/circles.clj
@@ -2,21 +2,42 @@
   (:use quil.core
         [quil.helpers.seqs :only [seq->stream range-incl]]))
 
+; the eventual goal is to make something like nodebeat
+; that is, a graph of sundry nodes that generate music.
+; steps:
+  ; draw a circle
+    ; check
+  ; draw two circles with a line between them
+    ; check
+  ; draw some kind of blip along the line
+    ; check
+  ; parameterize that shit
+    ; use the state stuff
+  ; iterate that shit
+    ; update the state in some reasonable way
+  ; implement node creation
+    ; probably just click to create nodes to start out 
+
+
+; window dimensions
 (def cx 400)
 (def cy 400)
 
 (defrecord Node
   [pos
    ;color
+   ;rate
+   ;kind
    ]
   )
 
 (defrecord Blip 
   [origin  ; origin node
    dest    ; destination node
-   pos     ; scalar position between nodes
+   pos     ; scalar position along edge between nodes
   ])
 
+; good ol' euclid
 (defn node-dist 
   [n1 n2]
   (let [[x1 y1]  (:pos n1)
@@ -24,7 +45,6 @@
     (sqrt (+ (pow (- x2 x1) 2)
              (pow (- y2 y1) 2)))))
 
-; need a function (Blip -> [x y])
 ; linear interpolation
 (defn blip-pos
   [blip]
@@ -32,8 +52,8 @@
         [dx dy] (:pos (:dest blip))
         ratio   (/ (:pos blip) 
                    (node-dist (:origin blip) (:dest blip))) ]
-    [(+ (* ratio (- dx ox)) ox) 
-     (+ (* ratio (- dy oy)) oy)]))
+    [(+ ox (* ratio (- dx ox))) 
+     (+ oy (* ratio (- dy oy)))]))
 
 (defn draw-blip
   [blip]
@@ -58,9 +78,10 @@
   ; blip
   (let [n1   (Node. [100 100]) 
         n2   (Node. [300 300]) 
-        blip (Blip. n1 n2 100)] 
+        blip (Blip. n1 n2 50)] 
     (draw-blip blip))
   )
+; how do i stop it from leaving residual artifacts from the blips?
 
 (defn setup []
   (frame-rate 24)
@@ -69,14 +90,20 @@
   (stroke 0)
   (stroke-weight 5)
   (fill 255 25)
-  )
+  (let [n1   (Node. [100 100]) 
+        n2   (Node. [300 300]) 
+        blip (Blip. n1 n2 200)] 
+    (set-state! {:nodes (atom [n1 n2]) 
+                 :blips (atom [blip])})
+    ))
 
 (defsketch simple-circle
-  :title "just some circles"
+  :title "circles and stuff"
   :setup setup
   :draw draw
   :size [cx cy]
   :keep-on-top true)
+
 
 (comment
   (use 'clojure.repl)
@@ -103,14 +130,6 @@
   (def dy (nth (:pos (:dest blip)) 1))
 
 
-  (+ (* ratio (- dx ox)) ox)
-
-
-
-  ; steps:
-  ; draw a circle
-  ; draw two circles with a line between them
-  ; draw some kind of blip along the line
   )
 
 ; it would be cool to bind a vim key to eval a sexpr
