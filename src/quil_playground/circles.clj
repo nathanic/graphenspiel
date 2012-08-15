d7936bf70e2da5253067075e0290fbec7ded56e6
Nathan Stien <nathanism@gmail.com>
2012-08-14 22:34:30 -0500

    sketches and notes

--- src/quil_playground/circles.clj
+++ src/quil_playground/circles.clj
@@ -13,6 +13,7 @@
     ; check
   ; parameterize that shit
     ; use the state stuff
+    ; check
   ; iterate that shit
     ; update the state in some reasonable way
   ; implement node creation
@@ -25,10 +26,59 @@
 
 (def node-radius 40)
 
+(comment
+  so this is really a digraph
+  and that should be reflected in my data structures
+  need to distinguish sources from sinks...
+
+  maybe two kinds of Nodes
+    sources and sinks
+    sources have pointers to their sinks
+    but then what of blips?
+
+  source nodes generate pulses periodically.
+  sink nodes receive any pulses and respond with an animation and sound,
+  pulses travel along graph edges at some speed 
+    uniform or variable? 
+  the nature of which can depend upon both the type of the source and that of
+  the sink.
+    could probably dispatch a multimethod on that
+
+  it would also be interesting to also have transceiver nodes that receive and
+  retransmit a modified pattern...
+    a rate divider
+    rate multiplier
+    signal gate
+    pulse stretcher?
+    pulse shrinker?
+  are my pulses discrete lengths or impulses? 
+
+  on each tick of the simulation
+    advance all pulses
+      (+ position (* pulse-rate delta-t))
+      could be fancy and schedule the handler for interpolated arrival time
+      actually, could do the whole thing that way
+      more declarative than imperative
+    respond to pulse arrivals at nodes
+      remove pulse
+      initiate next pulse
+      start a synth etc.
+      start an animation
+
+  does processing/quil make it easy to separate the simulation update
+  from the draw loop?
+  i guess i could do that anyway with another thread and some atoms/refs
+
+  it's funny that i'm reaching for OO
+  but OO has always been good for simulation
+)
+
+
 (defrecord Node
   [pos
+   ;sinks  ; list of Nodes connected as sinks
    ;color
-   ;rate
+   ;rate   ; rate of pulses in Hz
    ;kind
    ]
   )
@@ -42,8 +92,8 @@
 ; good ol' euclid
 (defn node-dist 
   [n1 n2]
-  (let [[x1 y1]  (:pos n1)
-        [x2 y2]  (:pos n2)] 
+  (let [[x1 y1] (:pos n1)
+        [x2 y2] (:pos n2)] 
     (sqrt (+ (pow (- x2 x1) 2)
              (pow (- y2 y1) 2)))))
 
@@ -75,13 +125,13 @@
     (ellipse x y node-radius node-radius)))
 
 
-; TODO: something less lame than (first (sort pts))
-; min doesn't directly work on vectors...
-(defn all-edges [nodes]
+; (set (map (comp sort (juxt :origin :dest)) nodes))
+; really need to clean up the blips vs edges confusion
+(defn all-edges [blips]
   (set (map (fn [n] 
               (sort [(:pos (:origin n)) 
                      (:pos (:dest n))])) 
-            nodes)))
+            blips)))
 
 (defn draw-edges [blips]
   (stroke 128 128 128)
@@ -107,13 +157,14 @@
         b1 (Blip. n1 n2 200) ; blips are complected with edges
         b2 (Blip. n1 n3 50)
         b3 (Blip. n2 n3 100) ] 
-    [:nodes (atom [n1 n2 n3]) 
-     :blips (atom [b1 b2 b3])]
+    {:nodes (atom [n1 n2 n3]) 
+     :blips (atom [b1 b2 b3])}
     ))
 
+
 (comment
-  (def nodes (:nodes (apply hash-map initial-state)))
-  (def blips (:blips (apply hash-map initial-state)))
+  (def nodes (:nodes initial-state))
+  (def blips (:blips initial-state))
   (prn @nodes)
   (prn @blips)
   (count (all-edges @blips))
@@ -127,10 +178,27 @@
   (stroke 0)
   (stroke-weight 5)
   (fill 255 25)
-  (apply set-state! initial-state)
+  (apply set-state! (apply concat initial-state))
   )
 
 (comment
+
+  (use 'clojure.repl)
+  (use 'clojure.pprint)
+
+  (defn calc-next-state 
+    [nodes blips]
+    [nodes (map #(update-in % [:pos] inc) blips)] 
+    )
+  (calc-next-state [] [(Blip. nil nil 1)])
+
+  (pprint initial-state)
+  (let [[n' b'] (calc-next-state @(:nodes initial-state) 
+                                 @(:blips initial-state))]
+    (reset! (:nodes initial-state) n')
+    (reset! (:blips initial-state) b')
+    )
+
 (defsketch some-nodes 
   :title "graphs and stuff"
   :setup setup
@@ -154,6 +222,9 @@
   (def blip (Blip. n1 n2 100)) 
   (node-dist n1 n2)
   (blip-pos blip)
+
+  (prn blip)
+  (update-in blip [:pos] inc)
 
   (def ratio (+ 100 (* 200 (/ 100 (node-dist n1 n2)))))
   (def ratio (/ (:pos blip) 
